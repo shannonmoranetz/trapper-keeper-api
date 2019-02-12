bc1a2cc415076f0fb9682536b868f401aee1d7e4
Dylan Hofmann <dylan.hofmann@gmail.com>
2019-02-12 11:30:49 -0700

    Set up server.js

--- server.js
+++ server.js
@@ -1,2 +1,11 @@
-const express = require('express');
+import express from 'express';
 const app = express();
+app.set('port', 3001);
+
+app.get('/', (req, res) => {
+  res.send('hello shannon!🐶')
+})
+
+app.listen(app.get('port'), () => {
+  console.log(`App is running on port: ${app.get('port')}`)
+})
