25a69bad512f82e722303e384157a2c6d0608729
shannonmoranetz <shannonrmoranetz@gmail.com>
2019-02-12 19:08:02 -0700

    Add get method for individual note and delete method for note

--- server.js
+++ server.js
@@ -9,6 +9,20 @@ app.get('/notes', (req, res) => {
   res.json(app.locals.notes);
 });
 
+app.get('/notes/:id', (req, res) => {
+  const note = app.locals.notes.find((note) => note.id == req.params.id);
+  if (!note) return res.status(404).json('Note not found.');
+  res.status(200).json(note);
+})
+
+app.delete('/notes/:id', (req, res) => {
+  const newNotes = app.locals.notes.filter((note) => note.id != req.params.id);
+  // sad path? 
+  app.locals.notes = newNotes;
+  res.status(204).json(`Note successfully deleted.`);
+  // review above
+})
+
 app.post('/notes', (req, res) => {
   const { id, title, list } = req.body;
   if (!id) return res.status(422).send('Error posting note.');
