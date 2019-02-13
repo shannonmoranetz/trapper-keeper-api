c74398972b9c37720360c9e970a3862fb85b453c
Dylan Hofmann <dylan.hofmann@gmail.com>
2019-02-13 15:41:07 -0700

    Update delete method and post method

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ import express from 'express';
 const app = express();
 app.use(express.json());
 app.set('port', 3001);
+import cors from 'cors';
+app.use(cors());
 
 app.locals.notes = [];
 
@@ -9,28 +11,29 @@ app.get('/notes', (req, res) => {
   res.json(app.locals.notes);
 });
 
-app.get('/notes/:id', (req, res) => {
-  const note = app.locals.notes.find((note) => note.id == req.params.id);
-  if (!note) return res.status(404).json('Note not found.');
-  res.status(200).json(note);
-})
+// We are not sure at this point if we want to use this? 
+// app.get('/notes/:id', (req, res) => {
+//   const note = app.locals.notes.find((note) => note.id == req.params.id);
+//   if (!note) return res.status(404).json('Note not found.');
+//   res.status(200).json(note);
+// })
 
 app.delete('/notes/:id', (req, res) => {
-  const newNotes = app.locals.notes.filter((note) => note.id != req.params.id);
-  // sad path? 
-  app.locals.notes = newNotes;
-  res.status(204).json(`Note successfully deleted.`);
-  // review above
+  const noteIndex = app.locals.notes.findIndex(note => note.id == req.params.id);
+  if (noteIndex === -1) return res.status(404).json('Note not found');
+  app.locals.notes.splice(noteIndex, 1);
+  return res.status(204).send('Note deleted');
 })
 
 app.post('/notes', (req, res) => {
-  const { id, title, list } = req.body;
+  const { id, title, noteItems } = req.body;
   if (!id) return res.status(422).send('Error posting note.');
-  // check if id exists too
+  const isduplicate = app.locals.notes.some(note => note.id == req.params.id)
+  if (isduplicate) return res.status(409).send('Please try adding your note again')
   const newNote = {
     title,
     id,
-    list
+    noteItems
   }
   app.locals.notes.push(newNote);
   res.status(200).json(app.locals.notes);
