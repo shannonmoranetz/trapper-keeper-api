c7cd1748e9a354099f754e76371b256b53a3f021
shannonmoranetz <shannonrmoranetz@gmail.com>
2019-02-16 17:35:03 -0700

    Add delete method to backend

--- app.js
+++ app.js
@@ -10,19 +10,12 @@ app.get('/notes', (req, res) => {
   res.status(200).json(app.locals.notes);
 });
 
-// We are not sure at this point if we want to use this? 
-// app.get('/notes/:id', (req, res) => {
-//   const note = app.locals.notes.find((note) => note.id == req.params.id);
-//   if (!note) return res.status(404).json('Note not found.');
-//   res.status(200).json(note);
-// })
-
 app.delete('/notes/:id', (req, res) => {
   const noteIndex = app.locals.notes.findIndex(note => note.id == req.params.id);
   if (noteIndex === -1) return res.status(404).json('Note not found');
   app.locals.notes.splice(noteIndex, 1);
-  return res.sendStatus(204)
-})
+  return res.sendStatus(204);
+});
 
 app.post('/notes', (req, res) => {
   const { id, title, noteItems } = req.body;
